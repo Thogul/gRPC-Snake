b85a792952c65989293bc90e900d3dccb22d3a24
Thogul <thomas@gulbrandsrod.no>
2021-05-05 14:15:20 +0200

    Testing == override

--- gameEngine/objects.py
+++ gameEngine/objects.py
@@ -13,6 +13,15 @@ class Bodypart():
     y: int
     skin: str = 'O'
 
+    def __eq__(self, other):
+        try:
+            xbool = other.x == self.head.x
+            ybool = other.y == self.head.x
+            return xbool and ybool
+        except:
+            return False
+
+
 if __name__ == '__main__':
     body = Bodypart(10, 11)
     print(body)
