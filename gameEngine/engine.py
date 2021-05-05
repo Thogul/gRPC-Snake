e14ae8cc901651e20449ae1e3009cb6c96501acd
Stian Furu <stian.furu97@gmail.com>
2021-05-05 16:17:27 +0200

    random spwan

--- gameEngine/engine.py
+++ gameEngine/engine.py
@@ -1,6 +1,8 @@
 from snake import Snake
 from objects import Food
 
+import random
+
 def clean_field(width=11, height=11):
     return [['*' for i in range(width)] for i in range(height)]
 
@@ -13,6 +15,14 @@ def insert_food(field, foods):
     for food in foods:
         field[food.y][food.x] = food.skin
 
+def spawn_food(field):
+    max_y = len(field)-1
+    max_x = len(field[0])-1
+    
+    random_x = random.randint(0, max_x)
+    random_y = random.randint(0, max_y)
+
+    return  [random_x, random_y]
 
 def print_field(field):
     for y in field:
@@ -22,7 +32,7 @@ def print_field(field):
 
 snake = Snake()
 field = clean_field()
-foods = [Food(2, 2)]
+foods = [Food(spawn_food(field)[0], spawn_food(field)[1])]
 while True:
     field = clean_field()
     insert_food(field, foods)
