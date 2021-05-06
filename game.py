0170e0b9c33f9732b1107e5a7ca2dc53bc7b8eb0
Margrethe Luth <s344101@oslomet.no>
2021-05-06 14:08:03 +0200

    Lagt til qt designer

--- game.py
+++ game.py
@@ -4,42 +4,56 @@ import sys
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QWidget, QFormLayout, QPushButton, QLineEdit, QColorDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Snok(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
 
-    def __init__(self):
-        super().__init__()
-
-        self.GUI()
-
-    def GUI(self):
-
-        self.gameBoard = Board(self)
-        self.setCentralWidget(self.gameBoard)
-
-        self.msgBar = self.statusBar()
-        self.gameBoard.msgScoreBar[str].connect(self.msgBar.showMessage)
-
-        self.msgBar.showMessage("Hello")
-
-        
-
-        self.setGeometry(100, 100, 1000, 800)
-        self.setStyleSheet("background: lightgreen;")
-        self.msgBar.setStyleSheet("background: #CD96CD;")
-        self.gameBoard.setStyleSheet("border: 10px dotted black;")
-
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1017, 788)
+        MainWindow.setStyleSheet("background: rgb(170, 255, 127)")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.board = QtWidgets.QFrame(self.centralwidget)
+        self.board.setGeometry(QtCore.QRect(0, 0, 1021, 741))
+        self.board.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.board.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.board.setObjectName("board")
+        self.scoreboard = QtWidgets.QTextBrowser(self.board)
+        self.scoreboard.setEnabled(True)
+        self.scoreboard.setGeometry(QtCore.QRect(825, 10, 171, 241))
+        self.scoreboard.setAutoFillBackground(False)
+        self.scoreboard.setStyleSheet("background: rgb(247, 247, 247, .5);\n"
+"")
+        self.scoreboard.setObjectName("scoreboard")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1017, 26))
+        self.menubar.setStyleSheet("background: rgb(238, 238, 238)")
+        self.menubar.setObjectName("menubar")
+        self.menuSettings = QtWidgets.QMenu(self.menubar)
+        self.menuSettings.setObjectName("menuSettings")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setStyleSheet("background: rgb(240, 240, 240)")
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuSettings.menuAction())
     
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        #Center
-        screen = QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move(int((screen.width() - size.width()) / 2),
-                  int((screen.height() - size.height()) / 2))
-    
-        self.setWindowTitle('Snok')
-        self.show()
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Snok"))
+        self.scoreboard.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.menuSettings.setTitle(_translate("MainWindow", "Settings"))
+
 
 class Board(QFrame):
 
@@ -108,16 +122,23 @@ class LogIn(QWidget):
 
 def onClicked(self):
     deleteWid = LogIn()
-    #deleteWid.close()
+    deleteWid.close()
     #deleteWid.setParent(None)
     deleteWid.deleteLater()
 def main():
-
-    app = QApplication([])
+    
+    app = QtWidgets.QApplication(sys.argv)
+    
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     lg = LogIn()
     lg.setAttribute(Qt.WA_DeleteOnClose)
-    #snok = Snok()
     sys.exit(app.exec_())
+    
+    
+   
 
 
 if __name__ == '__main__':
