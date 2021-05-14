2be5e1ae94266af8fa83e439433df257395717b9
Thogul <thomas@gulbrandsrod.no>
2021-05-14 15:56:53 +0200

    new ovject implementations implemented

--- engine_revised.py
+++ engine_revised.py
@@ -10,14 +10,34 @@ class Engine():
         self.foods: List[game.Food] = []
         self.walls: List[game.Object] = []
 
-    def __new_snake(self, id:str, x:int, y:int, score:int=0) -> game.Snake:
-        raise NotImplementedError
+    def __new_snake(self, id:str, x:int, y:int, score:int=0, length:int=5) -> game.Snake:
+        snake = game.Snake()
+        snake.id = id
+        snake.head.x = x
+        snake.head.y = y
+        snake.head.skin = '@'
+        for yoffset in range(1, length):
+            bodypart = game.Object()
+            bodypart.x = x
+            bodypart.y = snake.head.y - yoffset
+            bodypart.skin = 'O'
+            snake.body.append(bodypart)
+        return snake
 
     def __new_food(self, x:int, y:int, skin:str='A', strength:int=1) -> game.Food:
-        raise NotImplementedError
+        food = game.Food()
+        food.x = x
+        food.y = y
+        food.skin = skin
+        food.strength = strength
+        return food
 
     def __new_wall(self, x:int, y:int, skin='#') -> game.Object:
-        raise NotImplementedError
+        wall = game.Object()
+        wall.x = x
+        wall.y = y
+        wall.skin = '#'
+        return wall
 
     def set_snake_direction(self, id:str, direction:str) -> None:
         '''
