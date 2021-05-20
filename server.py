6855e8661332d448d4d5ecf6f5f39fa91865e877
Margrethe Luth <s344101@oslomet.no>
2021-05-20 15:08:09 +0200

    server og cient ssl

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ import protobuffer_pb2_grpc as rpc
 import db
 
 from engine_revised import Engine
+import signal
 
 class GameServer(rpc.GameServerServicer):
     def __init__(self, engine: Engine):
@@ -48,17 +49,32 @@ if __name__ == '__main__':
     engine.generate_outer_walls(50, 50)
     grpc_server = GameServer(engine)
 
+    with open('server.key', 'rb') as f:
+        private_key = f.read()
+    with open('server.crt', 'rb') as f:
+        certificate_chain = f.read()
+    
+    server_credentials = grpc.ssl_server_credentials(
+        ((private_key, certificate_chain,),))
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
     rpc.add_GameServerServicer_to_server(grpc_server, server)
 
     print('Starting server, listening...')
 
     #server.add_insecure_port('192.168.43.122:' + str(port))
-    server.add_insecure_port('[::]:' + str(port))
+    server.add_secure_port('[::]:' + str(port), server_credentials)
     server.start()
     gameloop = Thread(target=engine.game_loop_thread, daemon=True)
     gameloop.start()
     server.wait_for_termination()
+    try:
+        while True:
+            signal.pause()
+    except KeyboardInterrupt:
+        pass
+    server.stop(0)
     exit(0)
     '''
     id = 0
