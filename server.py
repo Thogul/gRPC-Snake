230d23fabfda469951b84a5027bcb6431182c199
Thogul <thomas@gulbrandsrod.no>
2021-05-23 09:24:52 +0200

    Prometheus logging done, might wanna setup grafana aswell :)

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+from logging import lastResort
 import sys
 from threading import Thread
 
@@ -12,6 +13,10 @@ import db
 
 from engine_revised import Engine
 import signal
+
+import prometheus_client
+import time
+import psutil
 
 class GameServer(rpc.GameServerServicer):
     def __init__(self, engine: Engine, db: db.DB):
@@ -43,6 +48,20 @@ class GameServer(rpc.GameServerServicer):
         high_scores.extend(self.db.get_scores())
         return high_scores
 
+def logging_thread():
+    UPDATE_PERIOD = 1 #1 sec update period
+    SYSTEM_USAGE = prometheus_client.Gauge('system_usage',
+                                        'Hold current system resource usage',
+                                        ['resource_type'])
+
+    prometheus_client.start_http_server(9999)
+    
+    while True:
+        print('logging stuff')
+        SYSTEM_USAGE.labels('CPU').set(psutil.cpu_percent())
+        SYSTEM_USAGE.labels('Memory').set(psutil.virtual_memory()[2])
+        time.sleep(UPDATE_PERIOD)
+
 
 if __name__ == '__main__':
     port = 50051
@@ -72,6 +91,10 @@ if __name__ == '__main__':
 
     rpc.add_GameServerServicer_to_server(grpc_server, server)
 
+    #start prometeus rescource logging(only cpu and memory, but should have been more)
+    #prometheus_thread = Thread(target=logging_thread, daemon=True)
+    #prometheus_thread.start()
+
     print('Starting server, listening...')
 
     #server.add_insecure_port('192.168.43.122:' + str(port))
@@ -79,7 +102,9 @@ if __name__ == '__main__':
     server.start()
     gameloop = Thread(target=engine.game_loop_thread, daemon=True)
     gameloop.start()
-    server.wait_for_termination()
+
+    logging_thread()
+    #server.wait_for_termination()
     '''
     try:
         while True:
