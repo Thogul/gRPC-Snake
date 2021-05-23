39ccaf083a391e951536a6f736e8ee67c0f0d33c
CamillaG1998 <camilla.gj64@gmail.com>
2021-05-23 11:48:14 +0200

    cleaning, and high score still bad

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from protobuffer_pb2_grpc import GameServer
+from grpc import Server
 import engine_revised
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import * 
@@ -317,19 +319,18 @@ class HighScoreWidget(QWidget):
         self.titlelabel.setObjectName("title")
         self.titlelabel.setGeometry(QtCore.QRect(230, 20, 150, 20))
         self.titlelabel.setStyleSheet("Font: 15pt")
-        self.label = QtWidgets.QLabel(Widget)   
-        self.label.setObjectName("scorelabel")
-        self.label.setGeometry(QtCore.QRect(270, 50, 130, 20 ))
-        self.label.setStyleSheet("Font: 12pt")
         self.scoreboard = QtWidgets.QTextEdit(Widget)
         self.scoreboard.setEnabled(False)
         self.scoreboard.setGeometry(QtCore.QRect(150, 100, 300, 300))
         self.scoreboard.setFont(QFont("Arial", 12))
         self.scoreboard.setStyleSheet("background: rgba(247, 247, 247, .5)")
         self.scoreboard.setObjectName("scoreboard")
+        self.engine = engine_revised.Engine()
+        self.client = Client(userName, self.engine)
+        string_score = self.client.get_high_scores()
         #self.client = Client()
         #self.client.
-        self.scoreboard.setPlainText()
+        self.scoreboard.setPlainText(string_score)
 
 
     
